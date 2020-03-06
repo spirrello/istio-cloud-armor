373820525988f482af775f2ec9cdc3908e4496f4
Stefano Pirrello <spirrello.com>
2020-03-06 16:59:00 -0500

    deleted istio service, patching instead

--- demoapp.sh
+++ demoapp.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+kubectl patch svc istio-ingressgateway -n istio-system --type json -p='[  {    "op": "add",    "path": "/metadata/annotations/cloud.google.com~1neg",    "value": "{\"ingress\": true}"  },  {    "op": "replace",    "path": "/spec/type",    "value": "NodePort"  },  {    "op": "remove",    "path": "/status"  },  {    "op": "add",    "path": "/metadata/annotations/beta.cloud.google.com~1backend-config",    "value": "{\"ports\": {\"80\":\"cloudarmor-staging-test\"}}"  }]'
 
 kubectl run nginx --image=nginx && k expose deploy/nginx --name nginx --type ClusterIP --port 80 --target-port 80
