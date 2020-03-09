13c374418a74e8181bcd9c58dd9916f4dc1877df
Stefano Pirrello <spirrello.com>
2020-03-09 10:37:26 -0400

    echo message

--- cert.sh
+++ cert.sh
@@ -27,6 +27,7 @@ CHECK_CERT=`openssl verify -verbose -CAfile $CA_CERT  $CERT_NAME`
 
 # Check if certificate is valid
 if [[ $CHECK_CERT == *"OK"* ]]; then
+  echo "Certificate is valid, creating K8s secret now..."
   kubectl create secret tls $K8S_SECRET --cert=$CERT_NAME --key=$CERT_KEY -n istio-system
 else
   echo "ERROR OCCURRED WHILE GENERATING CERTIFICATE"
