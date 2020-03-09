f18fd1a8491aecf112eb4b60622fdd85257fbcd1
Stefano Pirrello <spirrello.com>
2020-03-09 10:28:25 -0400

    updated cert sh, gitignore and renamed istio gateway file

--- cert.sh
+++ cert.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 
+#CA CERT
+ORG="example"
+CN="example.com"
+CA_CERT="ca-cert.crt"
+CA_KEY="ca.key"
+
+#CERT REQUEST
+CSR="nginx.example.com.csr"
+CERT_CN="nginx.example.com"
+CERT_KEY="nginx.example.com.key"
+CERT_NAME="nginx.example.com.crt"
+CERT_ORG="SOME ORG"
+K8S_SECRET="testsecret-tls"
+
 openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 \
--subj '/O=example Inc./CN=example.com' -keyout example.com.key -out example.com.crt
+-subj "/O=$ORG Inc./CN=$CN" -keyout $CA_KEY -out $CA_CERT
 
-openssl req -out nginx.example.com.csr -newkey rsa:2048 -nodes \
--keyout nginx.example.com.key -subj "/CN=nginx.example.com/O=some organization"
+openssl req -out $CSR -newkey rsa:2048 -nodes \
+-keyout $CERT_KEY -subj "/CN=$CERT_CN/O=$CERT_ORG"
 
-openssl x509 -req -days 365 -CA example.com.crt -CAkey example.com.key \
--set_serial 0 -in nginx.example.com.csr -out nginx.example.com.crt
+openssl x509 -req -days 365 -CA $CA_CERT -CAkey $CA_KEY \
+-set_serial 0 -in $CSR -out $CERT_NAME
+
+kubectl create secret tls $K8S_SECRET --cert=$CERT_NAME --key=$CERT_KEY
