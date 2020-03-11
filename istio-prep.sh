0ec61a4e77c4a14a8487d8644288707c48efde0c
Stefano Pirrello <spirrello.com>
2020-03-11 10:34:47 -0400

    echo ingress ip

--- istio-prep.sh
+++ istio-prep.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+
+#DEFAULTS
+SECURITY_POLICY="cloudarmor-test"
+PORT="80"
+SLEEP_TIME="60"
+
 echo "############## backing up istio-ingressgateway service ##############"
 kubectl -n istio-system get svc istio-ingressgateway -o yaml > BACKUP-istio-ingressgateway.yaml
 
@@ -7,8 +13,7 @@ echo "############## applying cloud armor backend config ##############"
 kubectl apply -f cloud-armor-backend.yaml
 
 echo "############## creating istio service patch ##############"
-SECURITY_POLICY="cloudarmor-test"
-PORT="80"
+
 # create patch to convert the service to type NodePort
 cat <<EOF > istio-ingress-patch.json
 [
@@ -41,3 +46,11 @@ kubectl -n istio-system patch svc istio-ingressgateway \
 
 echo "############## apply gateway settings and set up telemetry ##############"
 kubectl apply -f k8s/
+
+echo "############## Script completed successfully but it might take a few minutes for the HTTP LB to instantiate. Sleeping for $SLEEP_TIME seconds ##############"
+
+sleep $SLEEP_TIME
+
+INGRESS_IP=$(kubectl -n istio-system get ing istio-ingress -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+
+echo "INGRESS IP AND PORT: $INGRESS_IP:$PORT"
